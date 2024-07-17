fdddffb180e03a896d4838125bc547e4f97ee5e7
Matthew Bailey <mmchy6@gmail.com>
2024-07-16 22:12:06 -0600

    added logging and user input

--- linkToTxt/main.py
+++ linkToTxt/main.py
@@ -11,17 +11,30 @@ def url_get_simple_name(long_url: str) -> str:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
+    logging.info(f"Current working directory: {os.getcwd()}")
+
     # Clean up text files directory
     for file in os.listdir('text files'):
         full_path = os.path.join('text files', file)
         os.remove(full_path)
+        logging.info(f"Deleted file: {full_path}")
 
-    url = 'https://flowtrics.com/'
+    url = input("Enter a URL: ")
+
+    if not url.startswith('http'):
+        url = 'https://' + url
+
+    logging.info(f"Scraping links from URL: {url}")
 
     urls_to_scrape = scrape_links(url)
+    logging.info(f"Found {len(urls_to_scrape)} URLs to scrape")
 
     result_dict = scrape_text_from_urls(urls_to_scrape)
+    logging.info(f"Scraped text from {len(result_dict)} URLs")
 
     for url, text in result_dict.items():
-        new_file = open(f'text files/{url_get_simple_name(url)}.txt', 'w')
-        new_file.write(f'Scraped from URL: {url}\n\n----------\n\n{text}')
+        file_name = f'text files/{url_get_simple_name(url)}.txt'
+        with open(file_name, 'w') as new_file:
+            new_file.write(f'Scraped from URL: {url}\n\n----------\n\n{text}')
+
+        logging.info(f"Written scraped text to {file_name}")
